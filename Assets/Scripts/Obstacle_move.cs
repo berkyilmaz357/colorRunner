00c20cebf4b2204d768a4adea623770fad63c56c
Berk Yılmaz <berk.yilmaz.245@gmail.com>
2024-01-26 23:36:09 +0300

    add

--- Assets/Scripts/Obstacle_move.cs
+++ Assets/Scripts/Obstacle_move.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Obstacle_move : MonoBehaviour
 {
-    public float rotationSpeed = 5f; 
+    [SerializeField] public float rotationSpeed = 5f; 
 
     void Update()
     {
--- Assets/Scripts/PlaneColorChanger.cs
+++ Assets/Scripts/PlaneColorChanger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlaneColorChanger : MonoBehaviour
 {
-    private Color planeColor; 
+    [SerializeField] private Color planeColor; 
 
     private void Start()
     {
